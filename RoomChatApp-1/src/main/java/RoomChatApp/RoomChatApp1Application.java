b0ed06be8d0eb6168d46f6229ca494a2071f6240
abhaysingh28355 <147785453+abhaysingh28355@users.noreply.github.com>
2024-05-19 14:32:18 +0530

    Add files via upload
    init first

---
+++ RoomChatApp-1/src/main/java/RoomChatApp/RoomChatApp1Application.java
@@ -0,0 +1,13 @@
+package RoomChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomChatApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomChatApp1Application.class, args);
+	}
+
+}
---
+++ RoomChatApp-1/src/test/java/RoomChatApp/RoomChatApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package RoomChatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomChatApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
